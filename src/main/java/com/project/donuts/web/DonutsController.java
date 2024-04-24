3839515349cf157b3480e02a7e6f6d20e6ca17e6
Rajendran <rraj91091@gmail.com>
2024-04-24 15:02:46 +0800

    handling xml response type and internationalization

--- src/main/java/com/project/donuts/web/DonutsController.java
+++ src/main/java/com/project/donuts/web/DonutsController.java
@@ -4,13 +4,15 @@ import com.project.donuts.kafka.KafkaConstants;
 import com.project.donuts.kafka.MessagePublisher;
 import jakarta.validation.Valid;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Locale;
 import java.util.UUID;
 
 // @CacheConfig(cacheNames={"donuts"}) //possible alternative to CacheConfig class
@@ -20,13 +22,16 @@ public class DonutsController {
 
     private final DonutService donutService;
     private final MessagePublisher messagePublisher;
+    private final MessageSource messageSource;
 
-    public DonutsController(DonutService donutService, MessagePublisher messagePublisher) {
+    public DonutsController(DonutService donutService, MessagePublisher messagePublisher, MessageSource messageSource) {
         this.donutService = donutService;
         this.messagePublisher = messagePublisher;
+        this.messageSource = messageSource;
     }
 
-    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    // `produces` MediaTypes are optional unless we want to restrict the types to be returned
+    @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public ResponseEntity<Donut> createDonut(@Valid @RequestBody DonutDTO donut) {
         Donut newDonut = donutService.createDonut(donut);
@@ -42,13 +47,13 @@ public class DonutsController {
 
     // @CachePut(value="donuts", condition="#result.donuts.size()<50") // run function and cache results if condition is met
     @Cacheable("donuts")
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
     public Donuts getAllDonuts() {
         return new Donuts(donutService.getDonuts());
     }
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public Donut getDonut(@PathVariable(name = "id") String id) {
         Donut donut = donutService.getDonutById(UUID.fromString(id));
@@ -58,11 +63,18 @@ public class DonutsController {
         return donut;
     }
 
-    @PostMapping(value = "/send", produces = {MediaType.TEXT_PLAIN_VALUE})
+    @PostMapping(value = "/send")
     @ResponseStatus(value = HttpStatus.OK)
     public String sendDonuts(@RequestParam("message") String message) {
         messagePublisher.sendMessage(message, message, KafkaConstants.SEND_DONUTS_TOPIC);
         return "Send donuts message published successfully";
+    }
+
+    @GetMapping(value = "/internationalization")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String internationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("message.good-morning",null,"Default Message", locale);
     }
 
 }
